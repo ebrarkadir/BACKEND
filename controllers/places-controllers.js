4542134e2acdb4d8d303ed76e79deef657160995
ebrarkadir <cetinebrarkadir@gmail.com>
2023-12-30 18:35:25 +0300

    0.0.6

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,28 +1,31 @@
-const HttpError = require("../models/http-error");
+const { v4: uuidv4 } = require('uuid');
+
+
+const HttpError = require('../models/http-error');
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     location: {
       lat: 40.7484474,
-      lng: -73.9871516,
+      lng: -73.9871516
     },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
 ];
 
 const getPlaceById = (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
-  const place = DUMMY_PLACES.find((p) => {
+  const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
   });
 
   if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
+    throw new HttpError('Could not find a place for the provided id.', 404);
   }
 
   res.json({ place }); // => { place } => { place: place }
@@ -34,13 +37,13 @@ const getPlaceById = (req, res, next) => {
 const getPlaceByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find((p) => {
+  const place = DUMMY_PLACES.find(p => {
     return p.creator === userId;
   });
 
   if (!place) {
     return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
+      new HttpError('Could not find a place for the provided user id.', 404)
     );
   }
 
@@ -49,18 +52,21 @@ const getPlaceByUserId = (req, res, next) => {
 
 const createPlace = (req, res, next) => {
   const { title, description, coordinates, address, creator } = req.body;
+  // const title = req.body.title;
   const createdPlace = {
+    id: uuidv4(),
     title,
     description,
     location: coordinates,
     address,
-    creator,
+    creator
   };
 
-  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace) to add to the beginning of the array
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
 
-  res.status(201).json({ createdPlace });
+  res.status(201).json({place: createdPlace});
 };
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
