84729566115ad221a3e5abb06b9a95636d2edd26
ebrarkadir <cetinebrarkadir@gmail.com>
2024-01-03 01:03:25 +0300

    0.0.10

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes); // => /api/places...
-app.use('/api/users', usersRoutes); // => /api/users...
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -17,9 +17,14 @@ router.post(
     check("address").not().isEmpty(),
   ],
   placesControllers.createPlace
-); //express-validator
+);
 
-router.patch("/:pid", placesControllers.updatePlace);
+router.patch(
+  "/:pid",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  placesControllers.updatePlace
+);
+
 router.delete("/:pid", placesControllers.deletePlace);
 
 module.exports = router;
