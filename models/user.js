0aef82aa419e43e0a248e7876b6e56b562d4c5da
ebrarkadir <cetinebrarkadir@gmail.com>
2024-03-15 22:44:17 +0300

    user_added

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator'); // npm install --save mongoose-unique-validator
+
+const Shema = mongoose.Schema;
+
+
+const userSchema = new Shema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    places: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema); 
